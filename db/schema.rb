8e851586e00d87882c24fbf4bf7af14c8aae39db
finnporter <iamfinnporter@gmail.com>
2017-08-28 20:16:21 +0100

    user table migration doesn't work. no idea how to fix

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170828181143) do
+ActiveRecord::Schema.define(version: 20170828191044) do
 
   create_table "shows", force: :cascade do |t|
     t.string   "title"
